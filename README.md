efdfaccc9e7e81038da8ab005dde0dcb7777f828
U-Fantomas-PC\Fantomas <m.thomas.benard@gmail.com>
2016-05-31 22:00:02 +0200

    Added first version of the script.
    Handles whitespace removal and tabs replacement in the same place.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WhitespaceRemover
+
+The goal of this script is to simply remove empty spaces at the end of a file and replace tabs at the beginning of a line by spaces.
+You will need Ruby installed on your machine in order to make it work.
+
+Usage: ruby whitespace_remover.rb path_to_file number_of_spaces_for_tabs
+
+Enjoy !
---
+++ whitespace_remover.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+def remove_whitespace (filePath, tabSize)
+  file = File.open(filePath, "r")
+  tmpFilePath = "tmp"
+  tmpFile = File.open(tmpFilePath,"w+")
+  file.each_line do |line|
+    newLine = line.rstrip + $/
+    newLine = newLine.gsub(/^([\t])+/){ ' ' * tabSize * "#{$&}".size }
+    tmpFile.write(newLine)
+  end
+  file.close
+  tmpFile.close
+  FileUtils.mv(tmpFilePath,filePath)
+end
+
+if __FILE__ == $0
+  remove_whitespace(ARGV[0], ARGV[1])
+end
